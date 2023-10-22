5251d04ccc1221599cdb86fa87ceee0e2692125e
DK <danwangui66@gmail.com>
2023-10-22 16:18:52 +0300

    Fix Home Page and Stylesheet

--- js/custom.js
+++ js/custom.js
@@ -10,10 +10,10 @@
 
     // BACKSTRETCH SLIDESHOW
     $('.hero-section').backstretch([
-      "images/slideshow/afro-woman-cleaning-window-with-rag-home.jpg", 
-      "images/slideshow/afro-woman-holding-bucket-with-cleaning-items.jpg",
-      "images/slideshow/unrecognizable-cleaner-walking-into-hotel-room-with-tools-detergents.jpg"
-    ],  {duration: 2000, fade: 750});
+      "images/slideshow/education.avif", 
+      "images/slideshow/working.avif",
+      "images/slideshow/vis2.png"
+    ],  {duration: 3000, fade: 750});
     
     // CUSTOM LINK
     $('.smoothscroll').click(function(){
